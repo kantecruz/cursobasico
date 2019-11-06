df73d13677815529d68f684a3c85865bfa802cf3
ckante <carloskante@hotmail.com>
2019-11-06 13:20:03 -0600

    Agregar archivos de proyecto.

---
+++ Almacen/Controllers/ProductsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Almacen.Models;
+
+//este controlador es para los productos
+
+namespace Almacen.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private db_almacenEntities db = new db_almacenEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = db.product.Include(p => p.c_typeProduct);
+            return View(product.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_fktypeProduct = new SelectList(db.c_typeProduct, "id_typeProduct", "flddescription");
+            return View();
+        }
+
+        // POST: Products/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_product,fldname,fldprice,flddateOn,fldactive,id_fktypeProduct")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_fktypeProduct = new SelectList(db.c_typeProduct, "id_typeProduct", "flddescription", product.id_fktypeProduct);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_fktypeProduct = new SelectList(db.c_typeProduct, "id_typeProduct", "flddescription", product.id_fktypeProduct);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_product,fldname,fldprice,flddateOn,fldactive,id_fktypeProduct")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_fktypeProduct = new SelectList(db.c_typeProduct, "id_typeProduct", "flddescription", product.id_fktypeProduct);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.product.Find(id);
+            db.product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
